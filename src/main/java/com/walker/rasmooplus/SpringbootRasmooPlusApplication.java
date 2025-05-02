0adc114630f7fa1ad82f88f857f1e95e926287e9
walkerNegrão <walker.esteves.negrao@gmail.com>
2025-05-02 13:08:20 -0300

    Criação do repositório

---
+++ src/main/java/com/walker/rasmooplus/SpringbootRasmooPlusApplication.java
@@ -0,0 +1,13 @@
+package com.walker.rasmooplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRasmooPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRasmooPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walker/rasmooplus/SpringbootRasmooPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walker.rasmooplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRasmooPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
